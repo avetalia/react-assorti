3459437343eb76a98e07cc9690292ea99dd334c0
Natalia <natalia.averin@gmail.com>
2019-05-30 16:57:57 +0200

    dynamic content in jsx

--- src/spa-assorti/components/modal-button.js
+++ src/spa-assorti/components/modal-button.js
@@ -6,8 +6,39 @@ import { Icon, Box, Button, Text, Modal } from "@ui/elements";
 import { ICONS } from "@ui/ICONS";
 //Dynamic Component Names with JSX for Content in Modals
 
+export const ModalButton = ({ data }) => {
+  const [opened, setOpened] = useState(false);
+  const close = () => setOpened(() => false);
+  const toggle = () => setOpened(isOpen => !isOpen);
+
+  return (
+    <div>
+      <ZeroButton onClick={toggle}>
+        <Icon iconName={ICONS.TRASH} color="red" size={44} />
+      </ZeroButton>
+
+      {opened && (
+        <Modal onClose={close}>
+          <ContentSwitcherModal data={data} onClose={close} tag="standart" />
+        </Modal>
+      )}
+    </div>
+  );
+};
+
+ModalButton.propTypes = {
+  data: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired
+  })
+};
+
+const ZeroButton = styled.button`
+  background-color: transparent;
+  border: none;
+`;
+
 export const ModalContent = ({ data, onClose, onDeleteClick }) => {
-  console.log(data);
   return (
     <Box popup>
       <GridPopUp>
@@ -48,39 +79,6 @@ ModalContent.defaultProps = {
   onDeleteClick: () => {}
 };
 
-export const ModalButton = ({ data }) => {
-  const [opened, setOpened] = useState(false);
-  const close = () => setOpened(() => false);
-  const toggle = () => setOpened(isOpen => !isOpen);
-  console.log(data);
-
-  return (
-    <div>
-      <ZeroButton onClick={toggle}>
-        <Icon iconName={ICONS.TRASH} color="red" size={44} />
-      </ZeroButton>
-
-      {opened && (
-        <Modal onClose={close}>
-          <ModalContent data={data} onClose={close} />
-        </Modal>
-      )}
-    </div>
-  );
-};
-
-ModalButton.propTypes = {
-  data: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired
-  })
-};
-
-const ZeroButton = styled.button`
-  background-color: transparent;
-  border: none;
-`;
-
 const RightS = styled.div`
   display: flex;
   justify-content: flex-end;
@@ -100,3 +98,68 @@ const GridPopUp = styled.div`
 const Cell = styled.div`
   grid-area: ${({ gridArea }) => gridArea};
 `;
+
+//---------
+
+export const ContentSwitcherModal = ({ data, tag, close }) => {
+  const contentComponents = {
+    trash: ModalContentTrash,
+    update: ModalContentUpdate,
+    standart: ModalContent
+  };
+
+  const TagName = contentComponents[tag || "standart"];
+
+  return (
+    <div>
+      <TagName data={data} onClose={close} />
+      )}
+    </div>
+  );
+};
+
+export const ModalContentUpdate = ({ data, onClose }) => {
+  return (
+    <Box popup>
+      <h2>{data.title}</h2>
+      lets update something here!
+      <RightS>
+        <Button small onClick={onClose}>
+          <Icon iconName={ICONS.X} color="grey" />
+        </Button>
+      </RightS>
+      <Button onClick={onClose}>No, please cancel</Button>
+    </Box>
+  );
+};
+
+export const ModalContentTrash = ({ data, onClose }) => {
+  return (
+    <Box popup>
+      <GridPopUp>
+        <Cell gridArea="popupHeading">
+          <h2>{data.title}</h2>
+        </Cell>
+
+        <Cell gridArea="popupClose">
+          <RightS>
+            <Button small onClick={onClose}>
+              <Icon iconName={ICONS.X} color="grey" />
+            </Button>
+          </RightS>
+        </Cell>
+        <Cell gridArea="popupContent">
+          <Text>{data.text}</Text>
+          Call me to delete something, I am trash popup
+        </Cell>
+        <Cell gridArea="popupButtonYes">
+          <Button>Yes, delete</Button>
+        </Cell>
+
+        <Cell gridArea="popupButtonNo">
+          <Button onClick={onClose}>No, please cancel</Button>
+        </Cell>
+      </GridPopUp>
+    </Box>
+  );
+};
